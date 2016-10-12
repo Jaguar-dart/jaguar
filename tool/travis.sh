f1627e7dc509b1a04fbc37ec44f1d00ca2114a49
Kleak <segaud.kevin@gmail.com>
2016-10-12 18:38:51 +0200

    uncomment all + add chmod on travis.sh

--- tool/travis.sh
+++ tool/travis.sh
@@ -6,16 +6,16 @@ set -e
 $(dirname -- "$0")/ensure_dartfmt.sh
 
 # Run the tests.
-# pub run test
+pub run test
 
 # Run the build.dart file - just to make sure it works
 dart --checked tool/build.dart
 
 # Install dart_coveralls; gather and send coverage data.
-# if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
-#   pub global activate dart_coveralls
-#   pub global run dart_coveralls report \
-#     --retry 2 \
-#     --exclude-test-files \
-#     test/test_all.dart
-# fi
+if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report \
+    --retry 2 \
+    --exclude-test-files \
+    test/test_all.dart
+fi
