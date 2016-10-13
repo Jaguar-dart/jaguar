045dcfd2b8f25cb22b235203bc41d20b2d0b101b
Kleak <segaud.kevin@gmail.com>
2016-10-13 11:58:38 +0200

    rename file

--- test/test_all.dart
+++ test/test_all.dart
@@ -1,6 +1,6 @@
 import 'package:test/test.dart';
 
-import 'api_test.dart' as api_generator;
+import 'api_generation_test.dart' as api_generator;
 
 void main() {
   group('api', api_generator.main);
