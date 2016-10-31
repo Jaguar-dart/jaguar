11556c81281d587e2ca46999ff8dfbeee5714839
Ravi Teja Gudapati <tejainece@gmail.com>
2016-10-31 16:19:01 +0100

    Disabled all tests

--- test/test_all.dart
+++ test/test_all.dart
@@ -3,5 +3,5 @@ import 'package:test/test.dart';
 import 'api_generation_test.dart' as api_generator;
 
 void main() {
-  group('api', api_generator.main);
+  //TODO group('api', api_generator.main);
 }
