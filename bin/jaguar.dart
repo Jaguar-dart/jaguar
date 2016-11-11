677a920b1390ca05169b626dcb69cc8d6bbdc139
Ravi Teja Gudapati <tejainece@gmail.com>
2016-11-11 21:16:09 +0100

    Split generator into its own repo

--- bin/jaguar.dart
+++ bin/jaguar.dart
@@ -1,42 +1,7 @@
-import 'dart:io';
-import 'dart:convert';
+library jaguar.bin.jaguar;
 
-import 'package:build/build.dart';
-import 'package:yaml/yaml.dart';
-import 'package:jaguar/generator/phase/import.dart';
-
-String getBin() {
-  File pubspec = new File('./jaguar.yaml');
-  String content = pubspec.readAsStringSync();
-  var doc = loadYaml(content);
-  return doc['bin'] ?? 'bin/server.dart';
-}
-
-void launchWatch() {
-  Process process;
-  watch(phaseGroup(), deleteFilesByDefault: true)
-      .listen((BuildResult result) async {
-    if (result.status == BuildStatus.success) {
-      if (process != null) {
-        print("kill old server");
-        Process.killPid(process.pid);
-      }
-      String bin = getBin();
-      process = await Process.start('dart', [bin]);
-      process.stdout.transform(UTF8.decoder).listen(stdout.write);
-      process.stderr.transform(UTF8.decoder).listen(stderr.write);
-    }
-  });
-}
+import 'package:jaguar_generator/generator/generator.dart';
 
 main(List<String> args) {
-  if (args.length > 0) {
-    if (args[0] == 'watch') {
-      launchWatch();
-    } else if (args[0] == 'build') {
-      build(phaseGroup(), deleteFilesByDefault: true);
-    } else {
-      print("Invalid command!");
-    }
-  }
+  start(args);
 }
