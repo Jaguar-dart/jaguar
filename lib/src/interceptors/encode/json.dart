297c0ef5df6cf76874c21b6cb87cf9e4dfc6687d
Ravi Teja Gudapati <tejainece@gmail.com>
2017-01-10 18:45:58 +0100

    Added more Jsonable encoder interceptors

--- lib/src/interceptors/encode/json.dart
+++ lib/src/interceptors/encode/json.dart
@@ -1,5 +1,6 @@
 part of jaguar.src.interceptors;
 
+/// Interceptor wrapper to write Map or List as JSON response
 class WrapEncodeToJson implements RouteWrapper<EncodeToJson> {
   final String id;
 
@@ -22,6 +23,7 @@ class EncodeToJson extends Interceptor {
   }
 }
 
+/// Interceptor wrapper to write Map as JSON response
 class WrapEncodeMapToJson implements RouteWrapper<EncodeMapToJson> {
   final String id;
 
@@ -44,6 +46,7 @@ class EncodeMapToJson extends Interceptor {
   }
 }
 
+/// Interceptor wrapper to write List as JSON response
 class WrapEncodeListToJson implements RouteWrapper<EncodeListToJson> {
   final String id;
 
@@ -66,6 +69,7 @@ class EncodeListToJson extends Interceptor {
   }
 }
 
+/// Interceptor wrapper to write [ToJsonable] or List of [ToJsonable] as JSON response
 class WrapEncodeJsonable implements RouteWrapper<EncodeJsonable> {
   final String id;
 
@@ -80,9 +84,61 @@ class EncodeJsonable extends Interceptor {
   EncodeJsonable();
 
   @InputRouteResponse()
+  Response<String> post(Response incoming) {
+    Response<String> resp = new Response<String>.cloneExceptValue(incoming);
+    if (incoming.value is ToJsonable) {
+      resp.value = JSON.encode(incoming.value);
+    } else if (incoming.value is List<ToJsonable>) {
+      resp.value = JSON.encode(
+          incoming.value.map((ToJsonable obj) => obj.toJson()).toList());
+    }
+    resp.headers[HttpHeaders.CONTENT_TYPE] = 'application/json';
+    return resp;
+  }
+}
+
+/// Interceptor wrapper to write [ToJsonable] object as JSON response
+class WrapEncodeJsonableObject implements RouteWrapper<EncodeJsonableObject> {
+  final String id;
+
+  final Map<Symbol, MakeParam> makeParams = const {};
+
+  const WrapEncodeJsonableObject({this.id});
+
+  EncodeJsonableObject createInterceptor() => new EncodeJsonableObject();
+}
+
+class EncodeJsonableObject extends Interceptor {
+  EncodeJsonableObject();
+
+  @InputRouteResponse()
   Response<String> post(Response<ToJsonable> incoming) {
     Response<String> resp = new Response<String>.cloneExceptValue(incoming);
-    resp.value = incoming.value.toJson();
+    resp.value = JSON.encode(incoming.value.toJson());
+    resp.headers[HttpHeaders.CONTENT_TYPE] = 'application/json';
+    return resp;
+  }
+}
+
+/// Interceptor wrapper to write List of [ToJsonable] as JSON response
+class WrapEncodeJsonableList implements RouteWrapper<EncodeJsonableList> {
+  final String id;
+
+  final Map<Symbol, MakeParam> makeParams = const {};
+
+  const WrapEncodeJsonableList({this.id});
+
+  EncodeJsonableList createInterceptor() => new EncodeJsonableList();
+}
+
+class EncodeJsonableList extends Interceptor {
+  EncodeJsonableList();
+
+  @InputRouteResponse()
+  Response<String> post(Response<List<ToJsonable>> incoming) {
+    Response<String> resp = new Response<String>.cloneExceptValue(incoming);
+    resp.value = JSON
+        .encode(incoming.value.map((ToJsonable obj) => obj.toJson()).toList());
     resp.headers[HttpHeaders.CONTENT_TYPE] = 'application/json';
     return resp;
   }
