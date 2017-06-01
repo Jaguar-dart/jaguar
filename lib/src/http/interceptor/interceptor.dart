30a2b9bacf46389d734c4e51bde5b260302a4aec
tejainece <tejainece@gmail.com>
2017-06-01 12:59:44 +0100

    Jaguar 0.5.x primary changes

--- lib/src/http/interceptor/interceptor.dart
+++ lib/src/http/interceptor/interceptor.dart
@@ -1,21 +1,36 @@
 library jaguar.http.interceptor;
 
 import 'dart:async';
+import 'package:jaguar/jaguar.dart';
 
-abstract class RouteWrapper<WrapperType extends Interceptor> {
-  /// Id of the interceptor
+/// An interceptor wraps a route and performs an action before and
+/// after the route handler.
+///
+/// [pre] is the code that is executed before route handler
+///
+/// [post] is the code that is executed after route handler
+///
+/// [onException] is executed if there is an exception in the route chain.
+/// This can be used to clean up resources used by the interceptor.
+abstract class Interceptor<OutputType, ResponseType, InResponseType> {
+  /// Identifies an interceptor in the interceptor chain
   String get id => null;
 
-  Type get interceptorType => WrapperType;
-
-  const RouteWrapper();
-
-  WrapperType createInterceptor();
-}
-
-/// Base class for interceptor
-abstract class Interceptor<OutputType, ResponseType, InResponseType> {
   const Interceptor();
 
+  /// Shall be called before execution of route handler
+  /// \param ctx  Request context
+  /// \returns    Returns the output of the interceptor
+  FutureOr<OutputType> pre(Context ctx);
+
+  /// Shall be called after execution of route handler
+  /// \param ctx    Request context
+  /// \param inResp Incoming response
+  /// \returns      Transformed response
+  FutureOr<Response<ResponseType>> post(
+          Context ctx, Response<InResponseType> inResp) async => null;
+
+  /// Shall be called if there is an exception in the route chain.
+  /// This can be used to clean up resources used by the interceptor.
   Future<Null> onException() async {}
 }
