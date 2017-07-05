7a6b42a7378a4afa24ba0f0bfd2cd621966bf532
Ravi Teja Gudapati <tejainece@gmail.com>
2017-07-05 11:36:27 +0200

    export ErrorWriter

--- lib/src/serve/import.dart
+++ lib/src/serve/import.dart
@@ -11,5 +11,7 @@ import 'package:jaguar/src/serve/error_writer/import.dart';
 import 'package:args/args.dart';
 import 'package:yaml/yaml.dart';
 
+export 'package:jaguar/src/serve/error_writer/import.dart';
+
 part 'server.dart';
 part 'settings.dart';
