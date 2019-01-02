ce77f0728f9ecdba3c5c36e8511c7fb6bbf4e63e
tejainece <tejainece@gmail.com>
2019-01-02 17:15:42 +0100

    Generator: pub fixes

--- generator/lib/parser/parser.dart
+++ generator/lib/parser/parser.dart
@@ -2,7 +2,6 @@ library jaguar.generator.parser.route;
 
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/constant/value.dart';
-import 'package:source_gen/source_gen.dart';
 
 import '../common/constants.dart';
 
